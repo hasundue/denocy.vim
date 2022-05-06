0241900738f06eb44f1587b95003e722a0e5fc85
Shun Ueda <hasundue@gmail.com>
2022-05-06 13:00:22 +0900

    chore: cosmetic modification on tests

--- test.ts
+++ test.ts
@@ -1,41 +1,41 @@
 import { Denocy } from "./mod.ts";
 
-Denocy.test("test basic interfaces of Denocy", (vim) => {
+Denocy.test("test interface", (vim) => {
   vim.should.exist();
 });
 
-Denocy.test("test basic interfaces of Denocy with an option", { target: "any" }, (vim) => {
+Denocy.test("test interface with an option", { target: "any" }, (vim) => {
   vim.should.exist();
 });
 
 Denocy.test({
-  name: "test basic interfaces of Denocy with TestDefinition",
+  name: "test interface with TestDefinition",
   target: "any",
   fn: (vim) => {
     vim.should.exist();
   },
 });
 
-Denocy.test("test should.beNeovim for nvim", { target: "nvim" }, (nvim) => {
+Denocy.test("Denocy (nvim)", { target: "nvim" }, (nvim) => {
   nvim.should.beNeovim();
 });
 
-Denocy.test("test should.not.beNeovim for vim", { target: "vim" }, (vim) => {
+Denocy.test("Denocy (vim)", { target: "vim" }, (vim) => {
   vim.should.not.beNeovim();
 });
 
-Denocy.test("test BufferProvider", (vim) => {
+Denocy.test("BufferProvider", (vim) => {
   vim.buffer.should.exist();
   vim.buffer.should.beEmpty();
 });
 
-Denocy.test("test edit", (vim) => {
+Denocy.test("edit", (vim) => {
   vim.edit("./README.md");
   vim.should.not.beEmpty();
   vim.buffer.should.not.beEmpty();
 });
 
-Denocy.test("test containing", (vim) => {
+Denocy.test("containing", (vim) => {
   vim.edit("./README.md");
   vim.buffer.containing("denocy.vim").should.exist();
   vim.buffer.containing("denocy is the worst").should.not.exist();
