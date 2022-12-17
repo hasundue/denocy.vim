5709c428d27fda0cb4bd8809d933e4688f3a3ddb
denopendabot <denopendabot@github.com>
2022-12-17 00:19:15 +0000

    build(deps): bump denops_std from v3.11.1 to v3.12.0

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
 // Deno standard libraries
-export { assert, assertEquals, AssertionError } from "https://deno.land/std@0.168.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.168.0/async/mod.ts";
+export { assert, assertEquals, AssertionError } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.167.0/async/mod.ts";
 
 
 // Denops standard module
-export type { Denops } from "https://deno.land/x/denops_std@v3.11.1/mod.ts";
-export * as vim from "https://deno.land/x/denops_std@v3.11.1/function/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v3.11.1/helper/mod.ts";
-export { emit } from "https://deno.land/x/denops_std@v3.11.1/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.12.0/mod.ts";
+export * as vim from "https://deno.land/x/denops_std@v3.12.0/function/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v3.12.0/helper/mod.ts";
+export { emit } from "https://deno.land/x/denops_std@v3.12.0/autocmd/mod.ts";
 
 // Deno third party modules
 export { 
