506ca5be164fa7d4c2f5d293953a7c0b533a9efe
denopendabot <denopendabot@github.com>
2023-04-01 00:22:00 +0000

    build(deps): bump std from 0.181.0 to 0.182.0

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // Deno standard libraries
-export { assert, assertEquals, AssertionError } from "https://deno.land/std@0.181.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.181.0/async/mod.ts";
+export { assert, assertEquals, AssertionError } from "https://deno.land/std@0.182.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.182.0/async/mod.ts";
 
 
 // Denops standard module
