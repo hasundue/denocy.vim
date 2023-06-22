c1f07c22d99e95db2a5dacd309e2060c63b886a5
denopendabot <denopendabot@github.com>
2023-06-22 00:22:57 +0000

    build(deps): bump denops_std from v5.0.0 to v5.0.1

--- deps.ts
+++ deps.ts
@@ -4,10 +4,10 @@ export { delay } from "https://deno.land/std@0.192.0/async/mod.ts";
 
 
 // Denops standard module
-export type { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
-export * as vim from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v5.0.0/helper/mod.ts";
-export { emit } from "https://deno.land/x/denops_std@v5.0.0/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+export * as vim from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
+export { emit } from "https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts";
 
 // Deno third party modules
 export { 
